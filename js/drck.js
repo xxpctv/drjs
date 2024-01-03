34524c13672f461aed622a7d227611856606d869
xxpctv <105583466+xxpctv@users.noreply.github.com>
2024-01-03 13:43:32 +0800

    Update drck.js

--- js/drck.js
+++ js/drck.js
@@ -4,7 +4,7 @@
 {"url":"clan://localhost/xt/mzy.json","name":"bd"},
 {"url":"clan://localhost/xt/XP.json","name":"XP"},
 {"url": "https://ghproxy.net/https://raw.githubusercontent.com/FongMi/CatVodSpider/main/json/config.json", "name": "FongMi" },
-{"url": "https://codeberg.org/xxpctv/n/raw/branch/main/py/py_xc.py","name": "xc"},
+{"url": "https://ghproxy.net/https://raw.githubusercontent.com/xxpctv/MusicF/main/py/py_xc.py","name": "xc"},
 {"url": "https://notabug.org/ygfxz/6/raw/main/000.m3u8","name":"000" },
 {"url": "https://weixine.net/ysc.json","name": "ysc"},
 {"url":"https://agit.ai/kvymin/TV/raw/branch/master/Pandora.json","name":"Pandora"},
@@ -12,10 +12,8 @@
 {"url": "http://饭太硬.top/tv","name": "饭太硬"},
 {"url": "https://yydsys.top/duo","name": "duo"},
 {"url": "https://pastebin.com/raw/5NHaxyGR", "name": "js0"},
-{"url":"https://codeberg.org/xxpctv/ck/raw/branch/main/py/py_mzy.py","name":"mz"},
 {"url": "https://notabug.org/xxpctv/diy/raw/master/mao/py.py","name": "央视py"},
 {"url":"http://我不是.肥猫.love:63/接口禁止贩卖","name":"肥猫"},
-{"url": "https://codeberg.org/xxpctv/ck/raw/branch/main/py/drpy%20%282%29.js", "name": "drpy" },
 {"url":"https://drpyxxpctv-1.xxpctv.repl.co/config/2?ver=0","name":"drpypc"}
 
 
