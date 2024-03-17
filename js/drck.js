5b2bf54faba11d9f6e84e16085c3eb357f626373
xxpctv <105583466+xxpctv@users.noreply.github.com>
2024-03-17 13:28:38 +0800

    Add files via upload

--- js/drck.js
+++ js/drck.js
@@ -3,18 +3,22 @@
 {"url":"https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json","name":"XC"},
 {"url":"clan://localhost/xt/mzy.json","name":"bd"},
 {"url":"clan://localhost/xt/XP.json","name":"XP"},
-{"url": "https://ghproxy.net/https://raw.githubusercontent.com/FongMi/CatVodSpider/main/json/config.json", "name": "FongMi" },
+{"url": "https://agit.ai/1018610504/yg/raw/branch/main/box%e5%8e%9f.json", "name": "宝盒" },
 {"url": "https://ghproxy.net/https://raw.githubusercontent.com/xxpctv/MusicF/main/py/py_xc.py","name": "xc"},
 {"url": "https://notabug.org/ygfxz/6/raw/main/000.m3u8","name":"000" },
-{"url": "https://weixine.net/ysc.json","name": "ysc"},
-{"url":"https://agit.ai/kvymin/TV/raw/branch/master/Pandora.json","name":"Pandora"},
+{"url": "https://agit.ai/n3rd/N3RD/raw/branch/master/JN/lemj.json","name": "lemj"},
+{"url":"http://xhww.fun/小米/DEMO.json","name":"小米"},
 {"url":"https://agit.ai/xxpctv/m/raw/branch/master/py/py_dz.py","name":"dz"},
 {"url": "http://饭太硬.top/tv","name": "饭太硬"},
 {"url": "https://yydsys.top/duo","name": "duo"},
 {"url": "https://pastebin.com/raw/5NHaxyGR", "name": "js0"},
+{"url": "https://agit.ai/ystvy/yydsy/raw/branch/master/sub/ati.json", "name": "ystvy"},
+{"url": "https://agit.ai/1452635819/h/raw/branch/master/尤东风.json", "name": "东风"},
+{"url": "https://agit.ai/zhoujck/config/raw/branch/master/box", "name": "zhoujck"},
+{"url": "https://agit.ai/mimang/cccc/raw/branch/master/ccc.json", "name": "ccc"},
 {"url": "https://notabug.org/xxpctv/diy/raw/master/mao/py.py","name": "央视py"},
-{"url":"http://我不是.肥猫.love:63/接口禁止贩卖","name":"肥猫"},
-{"url":"https://drpyxxpctv-1.xxpctv.repl.co/config/2?ver=0","name":"drpypc"}
+{"url":"http://rihou.cc:88/荷城茶秀","name":"茶秀"},
+{"url":"https://agit.ai/zxwwp/TVBox/raw/branch/master/20221115.json","name":"zxwwp"}
 
 
 
