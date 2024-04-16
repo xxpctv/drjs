36352f7f11f147927671bae6e1ee284ade9cfb89
xxpctv <105583466+xxpctv@users.noreply.github.com>
2024-04-16 19:16:03 +0800

    Update fm.js

--- js/fm.js
+++ js/fm.js
@@ -1,2 +1,2 @@
-{"urls": [{"url": "https://pastebin.com/raw/5NHaxyGR","name": "道长"},{"url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json","name": "南风"},{"url": "https://agit.ai/xxpctv/m/raw/branch/master/py/py_dz.py","name": "DZ"},
+{"urls": [{"url": "https://pastebin.com/raw/5NHaxyGR","name": "道长"},{"url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json","name": "南风"},{"url": "https://notabug.org/xxpctv/dz/raw/master/py/py_dzn.py","name": "DZ"},
 {"url": "http://我不是.肥猫.love:63/接口禁止贩卖","name": "肥猫"},{"url":"https://ghproxy.net/https://raw.githubusercontent.com/xxpctv/MusicF/main/py/py_xc.py","name":"自用"},{"url": "https://agit.ai/zxwwp/TVBox/raw/branch/master/20241115.json","name":"zxwwp"},{"url": "http://xhww.fun:63/小米/DEMO.json", "name": "小米"}]}
