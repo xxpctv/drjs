54b03649cf39b541e7c64299ffbe37861f810389
xxpctv <105583466+xxpctv@users.noreply.github.com>
2024-04-17 08:30:39 +0800

    Add files via upload

--- js/fm.js
+++ js/fm.js
@@ -1,2 +1,8 @@
-{"urls": [{"url": "https://pastebin.com/raw/5NHaxyGR","name": "道长"},{"url":"http://rihou.cc:88/荷城茶秀","name":"茶秀"},{"url": "https://notabug.org/xxpctv/dz/raw/master/py/py_dzn.py","name": "DZ"},
-{"url": "http://我不是.肥猫.love:63/接口禁止贩卖","name": "肥猫"},{"url":"https://ghproxy.net/https://raw.githubusercontent.com/xxpctv/MusicF/main/py/py_xc.py","name":"自用"},{"url": "https://notabug.org/ygfxz/6/raw/main/000.m3u8","name":"000" }，{"url": "http://xhww.fun:63/小米/DEMO.json", "name": "小米"}]}
+{"urls": [{"url": "https://pastebin.com/raw/5NHaxyGR","name": "道长"},
+{"url": "https://raw.liucn.cc/box/m.json","name": "LiuB"},
+{"url": "https://notabug.org/xxpctv/dz/raw/master/py/py_dzn.py","name": "DZ"},
+{"url": "http://我不是.肥猫.love:63/接口禁止贩卖","name": "肥猫"},
+{"url":"https://ghproxy.net/https://raw.githubusercontent.com/xxpctv/MusicF/main/py/py_xc.py","name":"自用"},
+{"url": "https://download.kstore.space/download/2883/nzk/nzk0722.json","name":"DD"},
+{"url": "http://饭太硬.top/tv", "name": "fty"}
+]}
