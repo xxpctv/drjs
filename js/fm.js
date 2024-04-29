0fa14a5084630d7701b63122dd804cdd33d532a1
xxpctv <105583466+xxpctv@users.noreply.github.com>
2024-04-30 01:13:29 +0800

    Update fm.js

--- js/fm.js
+++ js/fm.js
@@ -2,7 +2,7 @@
 {"url": "https://raw.liucn.cc/box/m.json","name": "LiuB"},
 {"url": "https://notabug.org/xxpctv/dz/raw/master/py/py_dzn.py","name": "DZ"},
 {"url": "http://我不是.肥猫.love:63/接口禁止贩卖","name": "肥猫"},
-{"url":"https://ghproxy.net/https://raw.githubusercontent.com/xxpctv/MusicF/main/py/py_xc.py","name":"自用"},
+{"url":"https://gh.con.sh/https://raw.githubusercontent.com/xxpctv/MusicF/main/py/py_xc.py","name":"自用"},
 {"url": "https://download.kstore.space/download/2883/nzk/nzk0722.json","name":"DD"},
 {"url": "http://饭太硬.top/tv", "name": "fty"}
 ]}
