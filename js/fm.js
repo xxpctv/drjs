952c19742fadefcdc678e0605f71117325a4d46c
xxpctv <105583466+xxpctv@users.noreply.github.com>
2024-06-28 13:48:07 +0800

    Add files via upload

--- js/fm.js
+++ js/fm.js
@@ -3,8 +3,9 @@
 {"url": "https://pastebin.com/raw/5NHaxyGR","name": "道长"},
 {"url": "https://raw.liucn.cc/box/m.json","name": "LiuB"},
 {"url": "https://notabug.org/xxpctv/dz/raw/master/py/py_dzn.py","name": "DZ"},
-{"url": "http://我不是.肥猫.love:63/接口禁止贩卖","name": "肥猫"},
 {"url":"https://ghproxy.net/https://raw.githubusercontent.com/xxpctv/MusicF/main/py/py_xc.py","name":"自用"},
 {"url": "https://download.kstore.space/download/2883/nzk/nzk0722.json","name":"DD"},
+{"url": "https://ghproxy.net/https://raw.githubusercontent.com/zxwwp/TVBox/master/20221115.json", "name": "zxwwp"},
+{"url": "https://ghproxy.net/https://raw.githubusercontent.com/PizazzGY/TVBox/main/api.json", "name": "Piza2"},
 {"url": "https://ghproxy.net/https://raw.githubusercontent.com/yoursmile66/TVBox/main/XC.json","name":"XC"}
 ]}
