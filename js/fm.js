d6dcd524286481479fd4f1a6b8a2a651087d9a9d
xxpctv <105583466+xxpctv@users.noreply.github.com>
2024-06-30 17:00:27 +0800

    Add files via upload

--- js/fm.js
+++ js/fm.js
@@ -1,10 +1,10 @@
 {"urls": [
-{"url": "https://ghproxy.net/https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/%E5%BD%B1%E8%A7%86/api.json", "name": "Piza"},
+{"url": "https://github.moeyy.xyz/https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/%E5%BD%B1%E8%A7%86/api.json", "name": "Piza"},
 {"url": "https://pastebin.com/raw/5NHaxyGR","name": "道长"},
 {"url": "https://raw.liucn.cc/box/m.json","name": "LiuB"},
 {"url": "https://notabug.org/xxpctv/dz/raw/master/py/py_dzn.py","name": "DZ"},
-{"url":"https://ghproxy.net/https://raw.githubusercontent.com/xxpctv/MusicF/main/py/py_xc.py","name":"自用"},
+{"url":"https://github.moeyy.xyz/https://raw.githubusercontent.com/xxpctv/MusicF/main/py/py_xc.py","name":"自用"},
 {"url": "https://download.kstore.space/download/2883/nzk/nzk0722.json","name":"DD"},
-{"url": "https://ghproxy.net/https://raw.githubusercontent.com/PizazzGY/TVBox/main/api.json", "name": "Piza2"},
-{"url": "https://ghproxy.net/https://raw.githubusercontent.com/yoursmile66/TVBox/main/XC.json","name":"XC"}
+{"url": "https://github.moeyy.xyz/https://raw.githubusercontent.com/PizazzGY/TVBox/main/api.json", "name": "Piza2"},
+{"url": "https://github.moeyy.xyz/https://raw.githubusercontent.com/yoursmile66/TVBox/main/XC.json","name":"XC"}
 ]}
